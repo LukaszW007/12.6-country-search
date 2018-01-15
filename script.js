1846c85d8b29f963fdbdcdddc1ff822d00b5bdad
LukaszWiszniewski <wisznu07@gmail.com>
2018-01-15 21:40:11 +0100

    Added displaying list of countries

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
 
-$('#search').on('click',searchCountries())
+$('#search').on('click', searchCountries); //  or  $('#search').click(searchCountries);
 
-function searchCountries(){
+function searchCountries() {
     var countryName = $('#country-name').val();
-    if(!countryName.length) countryName = 'Poland';
+    if (!countryName.length) countryName = 'Poland';
     $.ajax({
         url: url + countryName,
         method: 'GET',
         success: showCountriesList
     });
-    
+
 }
-function showCountriesList () {
-    
+
+function showCountriesList(resp) {
+    countriesList.empty();
+    resp.forEach(function (item) {
+        // $('<>').addClass('country').appendTo(countriesList);
+        var country = item.name;
+        var countryItems = country + "-items";
+
+        $('<li>').text("Country:"+ country).addClass(country).appendTo(countriesList);
+        $('<ul>').addClass(countryItems).appendTo(country);                     // osadzanie listy w liscie nie dziala ?
+        $('<li>').text("Capitol:"+ item.capital).appendTo(countryItems);
+        $('<li>').text("Capitol:"+ item.region).appendTo(countryItems);
+    })
 }
